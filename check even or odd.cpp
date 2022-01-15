09679d3965cc8ba9ce867e3b8fb8d42b04f21f63
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-16 01:13:43 +0500

    commit

---
+++ check even or odd.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"Enter the num= ";
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<"Given number is a even number";
+	}
+	else
+	{
+		cout<<"Given number is a odd number";
+	}
+}
