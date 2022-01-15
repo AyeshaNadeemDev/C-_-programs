e2e4450380b9aa1d563ca546bd810351ff391a6b
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-15 22:13:24 +0500

    2nd commit

---
+++ average of num.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,z,avg;
+	cout<<"Enter the first num= ";
+	cin>>a;
+	cout<<"Enter the second num= ";
+	cin>>b;
+	cout<<"Enter the third num= ";
+	cin>>c;
+	z=a+b+c;
+	avg=z/3;
+	cout<<"Average of three num= "<<avg;
+}
