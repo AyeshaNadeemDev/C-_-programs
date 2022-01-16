8242ab6f67b7e95a13214c92803a761480a07b39
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-16 21:42:04 +0500

    commit

---
+++ vowels.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	char x;
+	cout<<"Enter a character= ";
+	cin>>x;
+	if(x=='a'||x=='e'||x=='i'||x=='o'||x=='u')
+	{
+		cout<<"given character  is a vowel";
+	}
+	else
+	{
+		cout<<"given character  is not a vowel";
+	}
+		
+}
