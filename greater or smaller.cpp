81f1ca593c1e1d38ca1f567c6d83b59eb62d791b
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-17 13:10:48 +0500

    commit

---
+++ greater or smaller.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	cout<<"Enter the first number= ";
+	cin>>a;
+	cout<<"Enter the second number= ";
+	cin>>b;
+	if(a>b)
+	{
+		cout<<"Greater number is= "<<a<<endl;
+		cout<<"Smaller number is= "<<b<<endl;
+	}
+	else
+	{
+		cout<<"Greater number is= "<<b<<endl;
+		cout<<"Smaller number is= "<<a<<endl;
+	}
+	
+}
