6665d76e7b8083b1d18f5027114db4b2f945634e
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-17 13:08:33 +0500

    commit

---
+++ equation.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	float a,b,c,d,x,y,z;
+	cout<<"Enter the first num= ";
+	cin>>a;
+	cout<<"Enter the second num= ";
+	cin>>b;
+	cout<< "Enter the third num= ";
+	cin>>c;
+	cout<<"Enter the fourth num= ";
+	cin>>d;
+	x=a+b;
+	y=x*c;
+	z=y/d;
+	cout<<"Result of equation= "<<z;
+}
