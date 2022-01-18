48cff2fbce11ae598fc9dd363e1d6641f1f6c13c
Ayesha <ayeshanadeem1980659@gmail.com>
2022-01-18 22:57:04 +0500

    commit

---
+++ nested if.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+	int a;
+	cout<<"Enter a number= ";
+	cin>>a;
+	if (a==0)
+	{
+		cout<<"Given number is zero"<<endl;
+	}
+    else if (a<20)
+	   {
+			cout<<"Given number is less than 20"<<endl;
+	   }
+	else if(a<50)
+	   {
+			cout<<"Given number is less than 50"<<endl;
+	   }
+    else if(a<100.)
+		{
+			cout<<"Given number is less than 100"<<endl;
+     	}
+}
